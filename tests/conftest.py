582f00020da6c9b46eb64de5126e254809a55393
Bruno Peixoto <brunolnetto@gmail.com>
2024-06-15 19:50:03 -0300

    tests/ tests for utilitaries and database

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
 import pytest
+from unittest import mock
 
 @pytest.fixture(scope="session")
 def test_client():
@@ -7,3 +8,19 @@ def test_client():
         yield client
         
 
+@pytest.fixture
+def mock_logging(mocker):
+  """Provides a mock object for the logging module."""
+  return mocker.patch('backend.setup.logging.logger.error') 
+
+
+@pytest.fixture
+def mock_create_engine():
+    with mock.patch('sqlalchemy.create_engine') as mock_engine:
+        yield mock_engine
+
+
+@pytest.fixture
+def mock_sessionmaker():
+    with mock.patch('sqlalchemy.orm.sessionmaker') as mock_session:
+        yield mock_session
