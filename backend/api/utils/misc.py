99f6b1521cd2c5712a1fadd822dabb4ae971821c
Bruno Peixoto <brunolnetto@gmail.com>
2024-06-16 11:31:12 -0300

    refac/ move string_to_json to backend/utils/misc.py

--- backend/api/utils/misc.py
+++
@@ -1,9 +0,0 @@
-import json
-import re
-
-def string_to_json(string: str) -> dict:
-    string=string.replace("'", '\"')
-    string = re.sub(r'\bNone\b', 'null', string)
-    
-    return json.loads(string)
-
