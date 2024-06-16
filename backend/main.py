e3d561f53d587436bf4654a4128413b3fa3e78bd
Bruno Peixoto <brunolnetto@gmail.com>
2024-06-16 18:31:14 -0300

    feat/ app on main to avoid ruff removal

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,10 @@
 
+from backend.api.app import app
 from backend.setup.config import settings
 from backend.setup.logging import logger
 
 # Log the application URL
 logger.info(f"App running on {settings.server_host}")
+
+if __name__ == "__main__":
+    app.run(host=settings.server_host, port=settings.server_port)
