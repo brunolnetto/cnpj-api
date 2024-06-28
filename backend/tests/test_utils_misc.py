1f38d73a9f2b2b2e03d70f3859870d0cbf13e07f
Bruno Peixoto <brunolnetto@gmail.com>
2024-06-28 09:45:30 -0300

    test: move repository utilitaries tests to separate file

--- backend/tests/test_utils_misc.py
+++ backend/tests/test_utils_misc.py
@@ -5,9 +5,6 @@ from json import JSONDecodeError
 
 from backend.app.utils.misc import (
     is_field_valid,
-    format_database_date,
-    format_cep,
-    format_phone,
     replace_spaces_on_list_tuple,
     replace_invalid_fields_on_list_tuple,
     replace_invalid_fields_on_list_dict,
@@ -102,40 +99,6 @@ def test_is_database_field_valid():
     assert not is_field_valid("NaT")  # Case-insensitive
     assert not is_field_valid("NULL")
     assert not is_field_valid("None")  # Case-insensitive
-
-
-def test_format_database_date():
-    """Tests the format_database_date function."""
-
-    assert format_database_date("20240613") == "13/06/2024"
-    assert format_database_date("invalid_date") is None
-    assert format_database_date("20240613", delimiter="/") == "13/06/2024"
-
-
-def test_format_phone():
-    """Tests the format_phone function."""
-
-    assert format_phone("12", "34567890") == "(12) 3456-7890"
-    assert format_phone("12", "34567890", ddd_ldelimiter="") == "12) 3456-7890"
-    assert format_phone("12", "34567890", ddd_rdelimiter="") == "(12 3456-7890"
-    assert format_phone("12", "34567890", phone_delimiter="") == "(12) 34567890"
-    assert (
-        format_phone(
-            "12", "34567890", ddd_rdelimiter="", ddd_ldelimiter="", phone_delimiter=""
-        )
-        == "12 34567890"
-    )
-
-    assert format_phone("nan", "invalid_phone") == ""
-
-
-def test_format_cep():
-    """Tests the format_cep function."""
-
-    assert format_cep("12345678") == "12.345-678"
-    assert format_cep(12345678.0) == "12.345-678"  # Accepts floats
-    assert format_cep("00000000") == "00.000-000"  # Leading zeros
-    assert format_cep("invalid_cep") == ""
 
 
 def test_replace_spaces_on_list_tuple():
