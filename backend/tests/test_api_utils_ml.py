0136214128544d9159a57f2436dc022ca0122631
Bruno Peixoto <brunolnetto@gmail.com>
2024-06-29 16:57:50 -0300

    test: Postman tests for CNPJ api

---
+++ backend/tests/test_api_utils_ml.py
@@ -0,0 +1,19 @@
+import pytest
+
+from backend.app.api.utils.ml import find_most_possible_tokens
+
+def test_find_most_possible_tokens():
+    user_names = ["New York City", "Los Angeles", "Springfield", "Smalltown"]
+    user_municipality = "New York"
+    limit_munic_count = 2
+    expected= ["New York City", "Smalltown"]
+
+    most_possible_cities = find_most_possible_tokens(
+        user_names, user_municipality, limit_munic_count
+    )
+    
+    assert most_possible_cities == expected
+
+
+    
+
