8881721f3746bf455118cf83638865a5dc1931ae
Bruno Peixoto <brunolnetto@gmail.com>
2024-07-06 21:05:15 -0300

    refactor: custom exceptions

--- backend/app/exceptions.py
+++ backend/app/exceptions.py
@@ -1,8 +1,23 @@
-from fastapi import HTTPException
+from fastapi import HTTPException, status
 
 class ExpiredTokenException(HTTPException):
     def __init__(self):
         super().__init__(
             status_code=401, 
             detail="Token has expired. Generate another token with application secret."
+        )
+
+class MissingTokenException(HTTPException):
+    def __init__(self):
+        super().__init__(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Token is missing",
+        )
+
+
+class InvalidTokenException(HTTPException):
+    def __init__(self):
+        super().__init__(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="Token is invalid",
         )
