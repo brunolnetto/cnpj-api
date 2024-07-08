64234995584a69d9c4e61029129de9c74da6f144
Bruno Peixoto <brunolnetto@gmail.com>
2024-07-08 09:45:56 -0300

    refactor: change order of routers

--- backend/app/api/routes/router_bundler.py
+++ backend/app/api/routes/router_bundler.py
@@ -6,7 +6,7 @@ api_router = APIRouter()
 
 # Add routers here
 api_router.include_router(auth.router)
-api_router.include_router(cnpj.router)
 api_router.include_router(setup.router)
+api_router.include_router(cnpj.router)
 
 
