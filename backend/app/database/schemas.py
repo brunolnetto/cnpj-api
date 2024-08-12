e8202c9d99f70d7507ef1a662e0f8fcaf1585f0c
Bruno Peixoto <brunolnetto@gmail.com>
2024-08-12 08:42:14 -0300

    lint

--- backend/app/database/schemas.py
+++ backend/app/database/schemas.py
@@ -3,7 +3,8 @@ from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
 
+
 class CNAE(Base):
-    __tablename__ = 'cnae'
+    __tablename__ = "cnae"
     codigo = Column(Integer, primary_key=True, index=True)
     descricao = Column(String)
