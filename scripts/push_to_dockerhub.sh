c16f5b27888bdfbe8f5aea2cb4307f2f5f2b5ca2
Bruno Peixoto <brunolnetto@gmail.com>
2024-09-18 16:22:41 -0300

    feat: push image to docker hub

---
+++ scripts/push_to_dockerhub.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+# Default values
+DOCKERHUB_USERNAME=""
+DOCKERHUB_PASSWORD=""
+IMAGE_NAME=""
+TAG="latest"
+
+# Print usage function
+usage() {
+    echo "Usage: $0 -u <dockerhub-username> -p <dockerhub-password> -i <image-name> -t <tag>"
+    echo "  -u, --username      Docker Hub username"
+    echo "  -p, --password      Docker Hub password"
+    echo "  -i, --image-name    Docker image name (default: conexxohub-site)"
+    echo "  -t, --tag           Docker image tag (default: latest)"
+    exit 1
+}
+
+# Parse command-line arguments using getopt
+OPTS=$(getopt -o u:p:i:t: --long username:,password:,image-name:,tag: -n 'parse-options' -- "$@")
+if [ $? != 0 ]; then
+    usage
+fi
+
+eval set -- "$OPTS"
+
+while true; do
+    case "$1" in
+    -u | --username )    DOCKERHUB_USERNAME="$2"; shift 2 ;;
+    -p | --password )    DOCKERHUB_PASSWORD="$2"; shift 2 ;;
+    -i | --image-name )  IMAGE_NAME="$2"; shift 2 ;;
+    -t | --tag )         TAG="$2"; shift 2 ;;
+    -- ) shift; break ;;
+    * ) break ;;
+    esac
+done
+
+# Validate required arguments
+if [ -z "$DOCKERHUB_USERNAME" ]; then
+    echo "Error: Docker Hub username is required."
+    usage
+fi
+
+# Docker login
+echo "[1] Logging in to Docker Hub..."
+echo "$DOCKERHUB_PASSWORD" | docker login -u "$DOCKERHUB_USERNAME" --password-stdin 
+if [ $? -ne 0 ]; then
+    echo "Docker login failed. Please check your credentials."
+    exit 1
+fi
+
+# Build the Docker image
+echo "[2] Building the Docker image..."
+docker build -t "$IMAGE_NAME" .
+
+# Tag the Docker image
+echo "[3] Tagging the Docker image..."
+docker tag $IMAGE_NAME $DOCKERHUB_USERNAME/$IMAGE_NAME:$TAG
+
+# Push the Docker image to Docker Hub
+echo "[4] Pushing the Docker image to Docker Hub..."
+docker push $DOCKERHUB_USERNAME/$IMAGE_NAME:$TAG
+
+# Check if the push was successful
+if [ $? -eq 0 ]; then
+    echo "Docker image pushed successfully!"
+else
+    echo "Failed to push the Docker image."
+    exit 1
+fi
